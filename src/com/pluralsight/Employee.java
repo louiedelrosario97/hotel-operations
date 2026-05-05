3529af85e79933157a9f6df0f439ea3c20b42c17
Louie D <louie.delrosario97@gmail.com>
2026-05-05 14:40:57 -0700

    Completed Exercise 2. Created new methods in Room. Created new methods in Employee.

--- src/com/pluralsight/Employee.java
+++ src/com/pluralsight/Employee.java
@@ -53,4 +53,3 @@ public class Employee
         this.hoursWorked += punchOut - punchIn;
     }
 }
-
--- src/com/pluralsight/Main.java
+++ src/com/pluralsight/Main.java
@@ -4,8 +4,10 @@ public class Main {
     static void main()
     {
     Employee Justin = new Employee("67", "Justin", "Manager", 50, 6);
-
     System.out.println(Justin.getHoursWorked());
+
+
+    // New shift
     Justin.punchTimeCard(10, 11);
     System.out.println(Justin.getHoursWorked());
 
